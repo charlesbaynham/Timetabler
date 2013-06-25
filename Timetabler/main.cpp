4da4d856dfeaff025393e3d119b063dea90412bc
Charles Baynham <bayners123@gmail.com>
2013-06-25 17:22:14 +0100

    framework

--- Timetabler/main.cpp
+++ Timetabler/main.cpp
@@ -8,77 +8,111 @@
 
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
-class person {
-public:
-    char* name;
-    int age;
-    bool female;
+using namespace std;
 
-    person(char* newname, int newage, bool newfemale):
-        name(newname),
-        age(newage),
-        female(newfemale)
-    {}
-};
 
-class house {
-    int _doors;
-    int _windows;
-    char* _name;
+class config {
+    float _probMutate = 0.03;
+    float _probMix = 0.2;
+    float _mixMax = 1;
     
-    std::vector<person> _inhabs;
+    float _searchMin = -50;
+    float _searchMax = 50;
     
 public:
-    house(int noDoors, int noWindows, char* houseName) :
-        _doors(noDoors),
-        _windows(noWindows),
-        _name(houseName)
-    {}
+    void setProbMutate(float prob) { _probMutate = prob; }
+    void setProbMix(float prob) { _probMix = prob; }
+    void setMixMax(float dist) { _mixMax = dist; }
+    void setSearchMin(float min){ _searchMin = min; }
+    void setSearchMax(float max){ _searchMax = max; }
+    float getProbMutate(){return _probMutate;}
+    float getProbMix(){return _probMix;}
+    float getMixMax(){return _mixMax;}
+    float getSearchMin(){return _searchMin;}
+    float getSearchMax(){return _searchMax;}
+};
+
+class chromosone {
+    float _fitness;
+    float _x;
+    float _y;
+    config* _myconfig;
     
-    
-    int getWindows() {
-        return _windows;
+    float _fnEval(float x, float y){
+        return ( (x-5.2)*(x-5.2) + (y-6)*(y-6) );
     }
     
-    person& addInhab(char* newname, int newage, bool newfemale)
-    {
-        person newguy(newname, newage, newfemale);
-        _inhabs.push_back(newguy);
-        return (_inhabs.back());
+    void _doMutate(){
+        //
     }
     
-    char* name() { return _name; }
-    
-    int newestAge(){
-        return _inhabs.back().age;
+    chromosone* _doMix(chromosone* c){
+        //
+        return NULL;
     }
-    person newestPerson(){ return _inhabs.back(); }
-    person oldestPerson(){ return _inhabs.front(); }
     
+public:
+    
+    chromosone(float x, float y, config* theconfig):
+    _x(x),
+    _y(y),
+    _myconfig(theconfig)
+    { _fitness = 0; }
+    
+    void mutate(){
+        float random = (float)rand() / RAND_MAX;
+        if (random <= _myconfig->getProbMutate()) {
+            _doMutate();
+        }
+    }
+    
+    chromosone* mix(chromosone* c) {
+        float random = (float)rand() / RAND_MAX;
+        if (random <= _myconfig->getProbMix()) {
+            _doMix(c);
+        }
+        return NULL;
+    }
+    
+    float updateFitness() {
+        _fitness = _fnEval(_x,_y);
+        return _fitness;
+    }
+};
+
+class algorithm {
+
+    vector<chromosone> _population;
+    config mainConfig;
+    
+public:
+
+    void start() {
+        //
+    }
+    void stop() {
+        //
+    }
 };
 
 
-int main(int argc, const char * argv[])
-{
+int main() {
+    
+//    srand(time(NULL));
+    
+    cout << "RAND_MAX is " << RAND_MAX << endl;
+    cout << "Some random numbers: " << rand() << "  " << rand() << "  " << rand() << "  " << endl;
+    
+    config theConfig;
+    
+    chromosone test(1,32.1,&theConfig);
+    
+    test.mutate();
+    theConfig.setProbMix(0.4);
+    test.mutate();
 
-    // insert code here...
-    std::cout << "Hello, World!\nI'm Charles\n";
-    
-    house WM = house(2, 10, "Wonastow Mill");
-    
-    person& added = WM.addInhab("Charles", 22, false);
-    WM.addInhab("Georgie", 19, true);
-    
-    std::cout << "My house, " << WM.name() << ", has " <<  WM.getWindows() << " windows.\n";
-    
-    std::cout << "We just added " << WM.newestPerson().name << " to the house. ";
-    if (WM.newestPerson().female) { std::cout << "She"; }
-    else { std::cout << "He";}
-    std::cout << " is " << WM.newestAge() << " years old.\n";
-    
-    std::cout << "There's also " << WM.oldestPerson().name << " who is " << WM.oldestPerson().age << " years old.\n";
-    
-    return 0;
 }
-
