70e0e75678b0ceddf6be0bbe7d2420a4d5ed6f02
Charles Baynham <bayners123@gmail.com>
2013-06-25 13:02:21 +0100

    Add person and streamline public functions of house

--- Timetabler/main.cpp
+++ Timetabler/main.cpp
@@ -7,21 +7,37 @@
 //
 
 #include <iostream>
+#include <vector>
+
+class person {
+    char* name;
+    int age;
+    bool female;
+};
 
 class house {
     int _doors;
     int _windows;
     char* _name;
     
+    std::vector<person> _inhabs;
+    
 public:
-    house(int noDoors, int noWindows, char* houseName) {
-        _doors = noDoors;
-        _windows = noWindows;
-        _name = houseName;
+    house(int noDoors, int noWindows, char* houseName) :
+        _doors(noDoors),
+        _windows(noWindows),
+        _name(houseName)
+    {}
+    
+    
+    int getWindows() {
+        return _windows;
     }
     
+    
+    char* name() { return _name; }
+    
 };
-
 
 
 int main(int argc, const char * argv[])
@@ -30,7 +46,11 @@ int main(int argc, const char * argv[])
     // insert code here...
     std::cout << "Hello, World!\nI'm Charles\n";
     
+    house WM = house(2, 10, "Wonastow Mill");
     
+
+    
+    std::cout << "My house, " << WM.name() << ", has " <<  WM.getWindows() << " windows.\n";
     
     return 0;
 }
