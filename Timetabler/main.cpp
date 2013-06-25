445fa997d42ccbec89772e4dab64a971e4ec17f4
Charles Baynham <bayners123@gmail.com>
2013-06-25 13:25:24 +0100

    Working house class containing vector of people inside it

--- Timetabler/main.cpp
+++ Timetabler/main.cpp
@@ -10,9 +10,16 @@
 #include <vector>
 
 class person {
+public:
     char* name;
     int age;
     bool female;
+
+    person(char* newname, int newage, bool newfemale):
+        name(newname),
+        age(newage),
+        female(newfemale)
+    {}
 };
 
 class house {
@@ -34,8 +41,20 @@ public:
         return _windows;
     }
     
+    person& addInhab(char* newname, int newage, bool newfemale)
+    {
+        person newguy(newname, newage, newfemale);
+        _inhabs.push_back(newguy);
+        return (_inhabs.back());
+    }
     
     char* name() { return _name; }
+    
+    int newestAge(){
+        return _inhabs.back().age;
+    }
+    person newestPerson(){ return _inhabs.back(); }
+    person oldestPerson(){ return _inhabs.front(); }
     
 };
 
@@ -48,9 +67,17 @@ int main(int argc, const char * argv[])
     
     house WM = house(2, 10, "Wonastow Mill");
     
-
+    person& added = WM.addInhab("Charles", 22, false);
+    WM.addInhab("Georgie", 19, true);
     
     std::cout << "My house, " << WM.name() << ", has " <<  WM.getWindows() << " windows.\n";
+    
+    std::cout << "We just added " << WM.newestPerson().name << " to the house. ";
+    if (WM.newestPerson().female) { std::cout << "She"; }
+    else { std::cout << "He";}
+    std::cout << " is " << WM.newestAge() << " years old.\n";
+    
+    std::cout << "There's also " << WM.oldestPerson().name << " who is " << WM.oldestPerson().age << " years old.\n";
     
     return 0;
 }
