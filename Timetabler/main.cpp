c40c47ea1d964aab82b9a8c8ed095c3620446dbe
Charles Baynham <bayners123@gmail.com>
2013-06-25 12:37:21 +0100

    Add house class

--- Timetabler/main.cpp
+++ Timetabler/main.cpp
@@ -8,11 +8,30 @@
 
 #include <iostream>
 
+class house {
+    int _doors;
+    int _windows;
+    char* _name;
+    
+public:
+    house(int noDoors, int noWindows, char* houseName) {
+        _doors = noDoors;
+        _windows = noWindows;
+        _name = houseName;
+    }
+    
+};
+
+
+
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
     std::cout << "Hello, World!\nI'm Charles\n";
+    
+    
+    
     return 0;
 }
 
