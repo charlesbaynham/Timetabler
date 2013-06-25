5f04c6147774bd3ddca6f9a8fff8d3af9ee054f8
Charles Baynham <bayners123@gmail.com>
2013-06-25 12:21:13 +0100

    Initial Commit

---
+++ Timetabler/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Timetabler
+//
+//  Created by Charles Baynham on 25/06/2013.
+//  Copyright (c) 2013 Charles Baynham. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
